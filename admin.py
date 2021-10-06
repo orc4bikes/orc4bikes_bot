2a132b2e7dd9b5911e42b913eef320a5848fdc77
orc4bikes <92050842+orc4bikes@users.noreply.github.com>
2021-10-07 01:55:02 +0800

    Create admin.py

---
+++ admin.py
@@ -0,0 +1,11 @@
+ADMIN_LIST = [
+    # Your personal chat_id here
+]
+
+# API token from telegram, to be retrieved from @botfather
+DEV_API_KEY = '' # API token for development purposes, to run in functions.py
+API_KEY = '' # API token for production
+
+# Chat ID for administrative purposes, a copy of rental logs will be sent here
+DEV_ADMIN_GROUP_ID = '' # group chat_id for development purposes
+ADMIN_GROUP_ID = '' # group chat_id for production
