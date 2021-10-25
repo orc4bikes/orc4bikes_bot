c749211c20873d2c04bf76526cafa957f97b1b81
Yew Chong <83965035+yewey2@users.noreply.github.com>
2021-10-26 00:47:57 +0800

    change namespace from functions to orc4bikes bot

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from functions import OrcaBot
+from Orc4bikesBot import Orc4bikesBot
 from admin import (
     ADMIN_GROUP_ID,
     API_KEY
     )
 
 if __name__=="__main__":
-    newbot = OrcaBot(API_KEY, admin_group_id=ADMIN_GROUP_ID, promo=True)
+    newbot = Orc4bikesBot(API_KEY, admin_group_id=ADMIN_GROUP_ID, promo=True)
     newbot.main()
